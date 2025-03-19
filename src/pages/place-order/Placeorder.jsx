a06a93243c5e459f41d3526745e8d2fd19edfbe9
kotharihimanshi <himanshikothari9@gmail.com>
2025-03-19 12:09:53 +0530

    as

--- src/pages/place-order/Placeorder.jsx
+++ src/pages/place-order/Placeorder.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import "./Placeorder.css"
-import { StoreContext } from '../../context/StoreContext'
+import { StoreContext } from '../../context/StoreContex'
 
 const Placeorder = () => {
 
